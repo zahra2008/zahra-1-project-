c6fffb1ddc907dc54b5d2aa0955cb13baeb2fd2e
zahra2008 <86829083+zahra2008@users.noreply.github.com>
2021-07-02 14:57:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var box;
+function setup() {
+  createCanvas(600,600);
+
+  box=createSprite(360,150,15,15);
+
+
+
+
+}
+
+function draw() 
+{
+  background("yellow");
+drawSprites();
+
+
+}
+
+
+
+
+
+
+
